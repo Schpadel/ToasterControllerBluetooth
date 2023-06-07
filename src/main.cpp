6e2a80bde7bb44ddcad62cbb41162ca0a8194093
Patrick Sandmann <sandmann.patrick@yahoo.de>
2023-06-07 15:22:50 +0200

    working gyro stuff

--- src/main.cpp
+++ src/main.cpp
@@ -23,40 +23,169 @@
  */
 
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <BleGamepad.h>
+#include <Wire.h>
 
 BleGamepad bleGamepad;
 
-// Last state of the buttons
+// Button stuff
 int lastButtonState[4] = {0, 0, 0, 0};
-int buttonPins[4] = {GPIO_NUM_23, GPIO_NUM_19, GPIO_NUM_5, GPIO_NUM_18};
+int buttonPins[4] = {GPIO_NUM_4, GPIO_NUM_19, GPIO_NUM_5, GPIO_NUM_18};
 int potiPin = GPIO_NUM_34;
+
+// Gyro stuff
+Adafruit_MPU6050 mpu;
 
 void setup()
 {
 
     // Initialize Button Pins
-    pinMode(GPIO_NUM_23, INPUT_PULLUP); // DREHEN links         X
+    pinMode(GPIO_NUM_4, INPUT_PULLUP); // DREHEN links         X
     pinMode(GPIO_NUM_19, INPUT_PULLUP); // DREHEN rechts        B
     pinMode(GPIO_NUM_5, INPUT_PULLUP);  // Bewegung nach unten   A
     pinMode(GPIO_NUM_18, INPUT_PULLUP); // Bewegung nach oben   Y
 
     // Potentiometer
-    //pinMode(potiPin, INPUT); // get analog data from potentiometer
+    // pinMode(potiPin, INPUT); // get analog data from potentiometer
 
     // Test old: 115200
     Serial.begin(9600);
+
+    // Start gyro stuff
+
+    // Try to initialize!
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+    Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange())
+    {
+    case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange())
+    {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth())
+    {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
+
+    Serial.println("");
+    delay(100);
+
+    // END gyro stuff
+
     Serial.println("Starting BLE work!");
-    bleGamepad.begin();
+    BleGamepadConfiguration bleGamepadConfig;
+    bleGamepadConfig.setAxesMax(2000);
+    bleGamepadConfig.setAxesMin(0);
+    bleGamepad.begin(&bleGamepadConfig);
 
     // The default bleGamepad.begin() above enables 16 buttons, all axes, one hat, and no simulation controls or special buttons
 }
+
+void readGyroSensor()
+{
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    
+    /* Print out the values */
+    /*
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+    */
+    //set joystick according to gyro data
+    int xMapped = map(a.acceleration.x, -8, 8, 0, 2000);
+    int yMapped = map(a.acceleration.y, -8, 8, 0, 2000);
+    bleGamepad.setAxes(xMapped, yMapped, 0, 0, 0, 0, 0, 0);
+    bleGamepad.sendReport();
+    
+}
+
 
 void loop()
 {
     if (bleGamepad.isConnected())
     {
 
+        // Gyro stuff
+        readGyroSensor();
         // Poti stuff
         int poti = analogRead(potiPin);
         int potiMapped = map(poti, 666, 863, 0, 1023);
@@ -64,7 +193,6 @@ void loop()
 
         // Button stuff
 
-        
         for (int index = 0; index < 4; index++)
         {
             int currentButtonState = !digitalRead(buttonPins[index]);
@@ -115,13 +243,6 @@ void loop()
                 }
                 lastButtonState[index] = currentButtonState;
             }
-            
         }
-
-        bleGamepad.setAxes(potiMapped, 0, 0, 0, 0, 0, 0, 0);
-
-        // Gyro sensor
-
-        delay(100);
     }
 }
